813d094e81df73449d79c99c8fd0fb9c15ffa0fc
Adebisi Joseph <adebisijoe@gmail.com>
2020-04-06 02:04:00 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { AntonymsComponent } from './antonyms/antonyms.component';
+import { HomonymsComponent } from './homonyms/homonyms.component';
+import { SynonymsComponent } from './synonyms/synonyms.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component: LoginComponent},
+  {path: 'register',component:RegisterComponent},
+  {path: 'antonyms',component:AntonymsComponent},
+  {path: 'homonyms',component:HomonymsComponent},
+  {path: 'synonyms',component:SynonymsComponent},
+  {path: 'home',component:HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
